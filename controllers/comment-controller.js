3380f545b87774fa542d027571e562c2f8b82095
Celia <celiamdona@gmail.com>
2022-04-29 21:36:11 -0400

    added routes for replies

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -22,6 +22,22 @@ const commentController = {
           })
           .catch(err => res.json(err));
     },
+
+    //add reply
+    addReply({params, body}, res){
+      Comment.findOneAndUpdate(
+        { _id: params.commentId},
+        {$push: {replies: body}},
+        {new: true}
+      )
+      .then(dbPizzaData => {
+        if (!dbPizzaData){
+          return res.status(404).json({message: 'No pizza found with this id!'});
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+    },
     
 
     // remove comment
@@ -45,7 +61,20 @@ const commentController = {
             res.json(dbPizzaData);
           })
           .catch(err => res.json(err));
+    },
+
+    // remove reply
+    removeReply({params}, res){
+        Comment.findOneAndUpdate(
+        { _id: params.commentId},
+        {$pull: {replies: {replyId: params.replyId}}},
+        {new: true}
+      )
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
     }
+
+
 };
 
 module.exports = commentController;
--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,60 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
-      },
-      commentBody: {
-        type: String
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now
+const ReplySchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment _id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+          getters: true
+        }
     }
+);
 
+
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+          getters: true,
+          virtuals: true
+        },
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
 });
 
 // create the Comment model using the CommentSchema
